8a4c7fd485c1f28590abb0bd4f3558691fdc3588
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-10-12 09:02:52 +0200

    Add packages for alrting

--- packages.el
+++ packages.el
@@ -58,3 +58,6 @@
 (package! guess-language)
 (package! websocket)
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+(package! org-alert :recipe (:host github :repo "spegoraro/org-alert" :files ("*.el")))
+(package! org-notifications :recipe (:host github :repo "doppelc/org-notifications" :files ("*.el" "sounds")))
+(package! pretty-agenda :recipe (:host github :repo "Artawower/.doom" :files ("pretty-agenda.el")))
