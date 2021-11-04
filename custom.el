6b81aec38c44c89e785318b2a00ba20c7674a515
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-04 06:56:03 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(ansi-color-names-vector
    ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
  '(custom-safe-themes
-   '("850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" default))
+   '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" default))
  '(exwm-floating-border-color "#32363b")
  '(fci-rule-color "#5B6268")
  '(highlight-tail-colors ((("#252d26" "#252d26") . 0) (("#212c32" "#212c32") . 20)))
@@ -14,6 +14,8 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#78bd65"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(objed-cursor-color "#ea3d54")
+ '(org-agenda-files
+   '("~/Documents/notes/roam/20211004193635-uczelnia.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE BIOD LAB. 1.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE PSB LAB. 1.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE PSB LAB. 2.org" "/home/pg/Documents/notes/PSB1.org" "/home/pg/Documents/notes/Podstawy modelowania matematycznego w inżynierii 1.org" "/home/pg/Documents/notes/bib.org" "/home/pg/Documents/notes/blog.org" "/home/pg/Documents/notes/feeds.org" "/home/pg/Documents/notes/grant_pci.org" "/home/pg/Documents/notes/notes.org" "/home/pg/Documents/notes/praktyki.org" "/home/pg/Documents/notes/statystyka_notatki_zaliczenie.org" "/home/pg/Documents/notes/struna2021.org" "/home/pg/Documents/notes/todo.org"))
  '(pdf-view-midnight-colors (cons "#cbccd1" "#1c1d20"))
  '(rustic-ansi-faces
    ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
