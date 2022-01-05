b513a1626f34aad9bdacef41dafc97693d89c938
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-01-05 13:24:18 +0100

    Enable deft

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
            ;;  +childframe)       ; a search engine for love and life
            (vertico +icons)
        :ui
-           ;; deft              ; notational velocity for Emacs
+           deft              ; notational velocity for Emacs
            doom                ; what makes DOOM look the way it does
            doom-dashboard      ; a nifty splash screen for Emacs
            doom-quit           ; DOOM quit-message prompts when you quit Emacs
