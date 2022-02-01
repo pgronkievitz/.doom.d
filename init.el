a0f796628f318e2bced13d9d9130fcff99f64673
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-01 21:35:51 +0100

    Support make and terraform

--- init.el
+++ init.el
@@ -92,13 +92,13 @@
            (lsp +peek)
            (magit
             +forge)            ; a git porcelain for Emacs
-           ;;make                ; run make tasks from Emacs
+           make                ; run make tasks from Emacs
            ;;pass              ; password manager for nerds
            pdf                 ; pdf enhancements
            ;;prodigy           ; FIXME managing external services & code builders
            rgb                 ; creating color strings
            ;;taskrunner        ; taskrunner for all your projects
-           ;;terraform         ; infrastructure as code
+           terraform         ; infrastructure as code
            ;;tmux              ; an API for interacting with tmux
            upload              ; map local to remote projects via ssh/ftp
        :os
