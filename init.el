1577b06788fdf14641eb03ad0a56ce7659aca692
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-08 22:47:16 +0100

    Remove `+fira` from ligatures

--- init.el
+++ init.el
@@ -25,8 +25,7 @@
            ;;hydra
            indent-guides       ; highlighted indent columns
            (ligatures
-           +extra
-           +fira)            ; ligatures and symbols to make your code pretty again
+           +extra)            ; ligatures and symbols to make your code pretty again
            ;;minimap           ; show a map of the code on the side
            (modeline)            ; snazzy, Atom-inspired modeline, plus API
            ;;nav-flash         ; blink cursor line after big motions
