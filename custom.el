9f699d7262049a061ca0d6d23a3e5d9f4a6292d1
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-21 19:59:03 +0100

    Move config to literate file

--- custom.el
+++ custom.el
@@ -3,58 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
- '(custom-safe-themes
-   '("36746ad57649893434c443567cb3831828df33232a7790d232df6f5908263692" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" default))
- '(exwm-floating-border-color "#32363b")
- '(fci-rule-color "#5B6268")
- '(highlight-tail-colors ((("#252d26" "#252d26") . 0) (("#212c32" "#212c32") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#fedd38"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#78bd65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
- '(objed-cursor-color "#ea3d54")
- '(org-agenda-files
-   '("~/Documents/notes/Praca_Inżynierska.org" "/home/pg/Documents/notes/aplikacje_bazodanowe_apex.org" "/home/pg/Documents/notes/health.org" "/home/pg/Documents/notes/aplikacje_bazodanowe.org" "/home/pg/Projects/private/nixos-configurations/README.org" "/home/pg/Documents/notes/todo.org" "/home/pg/Documents/notes/projects.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE BIOD LAB. 1.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE PSB LAB. 1.org" "/home/pg/Documents/notes/GRONKIEWICZ PATRYK SPRAWOZDANIE PSB LAB. 2.org" "/home/pg/Documents/notes/PSB1.org" "/home/pg/Documents/notes/Podstawy modelowania matematycznego w inżynierii 1.org"))
- '(pdf-view-midnight-colors (cons "#cbccd1" "#1c1d20"))
- '(rustic-ansi-faces
-   ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
- '(vc-annotate-background "#1c1d20")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#78bd65")
-    (cons 40 "#a4c756")
-    (cons 60 "#d1d247")
-    (cons 80 "#fedd38")
-    (cons 100 "#f8bc33")
-    (cons 120 "#f39b2e")
-    (cons 140 "#ee7b29")
-    (cons 160 "#dc7a54")
-    (cons 180 "#ca797f")
-    (cons 200 "#b978ab")
-    (cons 220 "#c9648e")
-    (cons 240 "#d95071")
-    (cons 260 "#ea3d54")
-    (cons 280 "#bf3e51")
-    (cons 300 "#94404f")
-    (cons 320 "#69424c")
-    (cons 340 "#5B6268")
-    (cons 360 "#5B6268")))
- '(vc-annotate-very-old-color nil)
- '(warning-suppress-log-types
-   '(((defvaralias losing-value woman-topic-history))
-     (org-load-hook)))
- '(warning-suppress-types
-   '((bug-reference)
-     ((defvaralias losing-value woman-topic-history))
-     (org-load-hook))))
+ '(warning-suppress-log-types '((initialization))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(line-number ((t (:inherit 'fixed-pitch))))
+ '(org-block ((t (:inherit 'fixed-pitch))))
+ '(org-code ((t (:inherit 'fixed-pitch))))
+ '(org-document-title ((t (:inherit 'variable-pitch :height 2.0))))
+ '(org-inline-src-block ((t (:inherit 'fixed-pitch))))
  '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
+ '(org-table ((t (:inherit 'fixed-pitch)))))
